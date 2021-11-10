9f51d69c5510e4b8b9ddf604c07696447267864c
Alois Sečkár <ellrohir@seznam.cz>
2021-11-10 19:30:20 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Master Coda
+ A website about programming
+
+Yoda icon by [Vectto]https://www.iconfinder.com/icons/2624871/jedi_jedi_master_starwars_yoda_icon
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>Master Coda</h1>
+        <p>Master Coda</p>
+        <img src="~/assets/master-coda.webp">
+    </div>
+</template>
