4bbf545daec3d82ce632a388dda6c42afbf485b1
Alois Sečkár <ellrohir@seznam.cz>
2021-11-11 19:46:43 +0100

    auto-deployment - last attempt?

--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,6 @@
         <h1>Master Coda</h1>
         <p>A website about programming in Nuxt3.</p>
         <img src="~/assets/master-coda.webp">
+        <p>&copy; 2021</p>
     </div>
 </template>
