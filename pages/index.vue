7ded10f26546fed9635cc44b8b62dd90b45a7a43
Alois Sečkár <ellrohir@seznam.cz>
2021-11-18 11:49:41 +0100

    auto-depolyment - test

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>Master Coda</h1>
-        <p>A website about programming in Nuxt3.</p>
+        <p>A website about programming in Nuxt3 (autodeployed to Firebase hosting).</p>
         <img src="~/assets/master-coda.webp">
         <p>&copy; 2021</p>
     </div>
