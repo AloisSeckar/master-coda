a8a73b95d55583add16bcbbfa53ac1d5b2c1654d
Alois Sečkár <ellrohir@seznam.cz>
2022-12-12 15:21:26 +0800

    updated npm packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
-// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-    buildModules: [
-        '@pinia/nuxt'
-    ],
     modules: [
         '@nuxtjs/tailwindcss',
+        '@pinia/nuxt',
     ]
 })
