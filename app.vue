3d677abd535029774973a435b3c4a1579b7ae0b6
Alois Sečkár <ellrohir@seznam.cz>
2022-12-15 23:56:53 +0100

    coding syntax highlight into tailwind

--- app.vue
+++ app.vue
@@ -13,36 +13,3 @@
 <script setup lang="ts">
 useArticleStore().fill()
 </script>
-
-<style>
-.code {
-  font-family: 'Courier New', monospace;
-  text-align: left;
-  background-color: #c8c8af;
-  color: black;
-}
-
-div.code {
-  padding: 0.5rem 1rem;
-}
-
-span.code {
-  padding: 0 0.2rem;
-}
-
-.j-key {
-  color: blue;
-}
-
-.j-var {
-  color: green;
-}
-
-.j-lit {
-  color: purple;
-}
-
-.j-com {
-  color: darkgray;
-}
-</style>
