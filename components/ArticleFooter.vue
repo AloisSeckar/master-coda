4deafefbcd4cbebcab6e480bf20745677471011d
Alois Sečkár <ellrohir@seznam.cz>
2022-12-17 13:05:47 +0100

    article footer

--- components/ArticleFooter.vue
+++ components/ArticleFooter.vue
@@ -1,5 +1,14 @@
 <template>
-    <div>
-        <div>Here will be common footer with links and stuff</div>
+    <div class="pt-4 text-xs">
+        <div class="text-amber-300">----------------------------------</div>
+        <div class="font-bold">Master Coda by Alois Sečkár</div>
+        <a class="block" :href="link.replace('##','blob')">Zobrazit zdrojový kód článku</a>
+        <a class="block" :href="link.replace('##','edit')">Navrhnout úpravu</a>
     </div>
 </template>
+
+<script setup lang="ts">
+const path = useRoute().path
+const id = path.substring(path.lastIndexOf('/') + 1)
+const link = `https://github.com/AloisSeckar/master-coda/##/master/pages/article/${id}.vue`
+</script>
--- components/CodaFooter.vue
+++ components/CodaFooter.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="m-1 mt-4 border-t-2 border-amber-500">
+    <div class="mt-4 border-t-2 border-amber-300">
         <div class="mt-1 text-right">&copy; 2022 <a href="http://alois-seckar.cz/">Alois Sečkár</a></div>
     </div>
 </template>
