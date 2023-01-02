e9931a3c7b4bf93d6a04a8ef9c38d44593968d14
Alois Sečkár <ellrohir@seznam.cz>
2023-01-02 19:52:54 +0100

    default Nuxt layout

--- app.vue
+++ app.vue
@@ -1,13 +1,7 @@
 <template>
-  <div class="flex justify-center">
-    <div class="w-4/5">
-      <CodaHeader />
-      <main>
-        <NuxtPage />
-      </main>
-      <CodaFooter />
-    </div>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
 
 <script setup lang="ts">
---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex justify-center">
+        <div class="w-4/5">
+            <CodaHeader />
+            <main>
+                <NuxtPage />
+            </main>
+            <CodaFooter />
+        </div>
+    </div>
+</template>
