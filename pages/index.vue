69f601e5ac27dd1a01c9ceae2645e97503fdc875
Alois Sečkár <ellrohir@seznam.cz>
2023-01-08 13:45:58 +0100

    index page content

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,18 @@
-<script setup>
-  const { data: content } = await useFetch('/api/articles');
-</script>
-
 <template>
-  <div>
-    <h1>Home page?</h1>
-    <h3>tailwind</h3>
-    <p>Text shall there be, think you?</p>
-    <h3>Articles</h3>
-    <div v-for="category in content" :key="category.cat">
-      <h4>{{ category.cat }}</h4>
-      <p>{{ category.dscr }}</p>
-      <ul>
-        <li v-for="article in category.articles" :key="article.id">
-          <NuxtLink :to="{ path: '/' + category.cat + '/' + article.id}">{{ article.title }}</NuxtLink>
-        </li>
-      </ul>
+    <div>
+        <h1>Master Coda</h1>
+        <p class="my-2">
+            Jmenuji se Alois Sečkár, živí (a&nbsp;baví) mě programování a&nbsp; věci, které jsem se přitom naučil,
+            si nechávat jen pro sebe.
+        </p>
+        <p class="my-2">
+            Na tomto blogu naleznete články o&nbsp;nejrůznějších IT tématech. Snad mé texty někomu pomohou. Budu rád
+            i&nbsp;za zpětnou vazbu, pokud v nich uvidíte chybu nebo znáte způsob, jak danou věc řešit lépe. Vždycky
+            je kam se zlepšovat.
+        </p>
+        <p class="mt-4 text-xs">
+           <strong>Pozn.:</strong> S účtem <NuxtLink to="https://twitter.com/MasterCoda" :external="true">MasterCoda</NuxtLink>
+           na Twitteru nemám nic společného.
+        </p>
     </div>
-  </div>
 </template>
