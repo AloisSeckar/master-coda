01ad9845a98ffdf606309e10122239fea70b949a
Alois Sečkár <ellrohir@seznam.cz>
2023-01-14 16:46:11 +0100

    total fun images count

--- pages/fun/index.vue
+++ pages/fun/index.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <h1 class="text-center">{{ random?.title }}</h1>
-        <div class="actionButton w-48 mx-auto" @click="funStore.getRandom()">
-            <div class="actionButtonText">NĚCO JINÉHO</div>
+        <div class="actionButton w-64 mx-auto hover:cursor-pointer" @click="funStore.getRandom()">
+            <div class="actionButtonText">NĚCO JINÉHO (je tu {{ total }} obrázků)</div>
         </div>
         <img class="mx-auto my-4" :src="'/fun/' + random?.id + '.jpg'" :alt="random?.title" />
     </div>
@@ -14,4 +14,5 @@ if (!funStore.loaded) {
     funStore.fill()
 }
 const random = computed(() => funStore.random)
+const total = computed(() => funStore.items.length)
 </script>
