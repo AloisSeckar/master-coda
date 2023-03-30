e2cf129555b3702a910370a8523c2b01c914a4bc
Alois Sečkár <ellrohir@seznam.cz>
2023-03-30 23:05:37 +0200

    no extra layouts

--- app.vue
+++ app.vue
@@ -1,7 +1,13 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div class="flex justify-center">
+    <div class="w-full md:w-4/5">
+      <CodaHeader />
+      <main>
+        <NuxtPage />
+      </main>
+      <CodaFooter />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
