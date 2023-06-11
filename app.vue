17a8bca25c2e2d2ca55d09650e21966bb510db7f
Alois Sečkár <ellrohir@seznam.cz>
2023-06-11 19:09:44 +0200

    social meta tags - last attempt...

--- app.vue
+++ app.vue
@@ -12,25 +12,14 @@
 
 <script setup lang="ts">
 useHead({
-  title: 'Master Coda - The Light Side of the Code',
-  meta: [
-    { name: 'og:type', content: 'website' },
-    { name: 'og:title', content: 'Master Coda' },
-    { name: 'og:description', content: 'Nepravidelný blog o programování' },
-    { name: 'og:image', content: 'https://master-coda.netlify.app/master-coda.webp' },
-    { name: 'og:url', content: 'https://master-coda.cz' },
-    { name: 'twitter:card', content: 'summary_large_image' }
-  ]
+  title: 'Master Coda - The Light Side of the Code'
 })
-/*
 useSeoMeta({
-  title: 'Master Coda',
-  ogTitle: 'Master Coda',
-  description: 'Nepravidelný blog o programování',
-  ogDescription: 'Nepravidelný blog o programování',
-  ogImage: 'https://master-coda.netlify.app/master-coda.webp',
-  twitterCard: 'summary_large_image'
+  ogTitle: () => 'Master Coda',
+  description: () => 'Nepravidelný blog o programování',
+  ogDescription: () => 'Nepravidelný blog o programování',
+  ogImage: () => 'https://master-coda.netlify.app/master-coda.webp',
+  twitterCard: () => 'summary_large_image'
 })
-*/
 useArticleStore().fill()
 </script>
--- pages/article/[article].vue
+++ pages/article/[article].vue
@@ -17,24 +17,11 @@
 const isNuxtTutorial = useRoute().params.article?.includes('nuxt')
 
 const article = useArticleStore().getById(useRoute().params.article as string)
-useHead({
-  meta: [
-    { name: 'og:type', content: 'website' },
-    { name: 'og:title', content: () => article?.title },
-    { name: 'og:description', content: () => article?.dscr },
-    { name: 'og:image', content: 'https://master-coda.netlify.app/master-coda.webp' },
-    { name: 'og:url', content: () => 'https://master-coda.cz/articles/' + article?.id },
-    { name: 'twitter:card', content: 'summary_large_image' }
-  ]
-})
-/*
 useSeoMeta({
-  title: article?.title,
-  ogTitle: article?.title,
-  description: article?.dscr,
-  ogDescription: article?.dscr,
-  ogImage: 'https://master-coda.netlify.app/master-coda.webp',
-  twitterCard: 'summary_large_image'
+  ogTitle: () => article?.title,
+  description: () => article?.dscr,
+  ogDescription: () => article?.dscr,
+  ogImage: () => 'https://master-coda.netlify.app/master-coda.webp',
+  twitterCard: () => 'summary_large_image'
 })
-*/
 </script>
