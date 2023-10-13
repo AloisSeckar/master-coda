bd57f4dc2e7b1c189589587665439405a2dc6d60
Alois Sečkár <ellrohir@seznam.cz>
2023-10-13 17:01:44 +0200

    Update index.vue
    Landing page text review

--- pages/index.vue
+++ pages/index.vue
@@ -3,19 +3,12 @@
     <h1>Master Coda</h1>
     <p class="my-2">
       Jmenuji se Alois Sečkár, živí (a&nbsp;baví) mě programování a&nbsp; věci, které jsem se přitom naučil,
-      si nechávat jen pro sebe.
+      si nechci nechávat jen pro sebe.
     </p>
     <p class="my-2">
       Na tomto blogu naleznete články o&nbsp;nejrůznějších IT tématech. Snad mé texty někomu pomohou. Budu rád
-      i&nbsp;za zpětnou vazbu, pokud v nich uvidíte chybu nebo znáte způsob, jak danou věc řešit lépe. Napište
+      i&nbsp;za zpětnou vazbu. Pokud v&nbsp;nich uvidíte chybu nebo znáte způsob, jak danou věc řešit lépe, napište
       mi na <a href="mailto:alois-seckar@gmail.com">alois-seckar@gmail.com</a>. Vždycky je kam se zlepšovat.
-    </p>
-    <p class="mt-4 text-xs">
-      <strong>Pozn.:</strong> S účtem
-      <NuxtLink to="https://twitter.com/MasterCoda" :external="true">
-        MasterCoda
-      </NuxtLink>
-      na Twitteru nemám nic společného.
     </p>
   </div>
 </template>
