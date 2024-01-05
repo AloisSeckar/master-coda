b38acf450c85fa44d5140f929558c6d9833e03ec
Alois Sečkár <ellrohir@seznam.cz>
2024-01-05 18:50:47 +0100

    feat: dynamic year in footer

--- components/CodaFooter.vue
+++ components/CodaFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-8 mt-4 mb-2 border-t-2 border-coda-green">
     <div class="my-1 mr-1 text-right">
-      &copy; 2023 <a href="http://alois-seckar.cz/">Alois Sečkár</a>
+      &copy; {{ new Date().getFullYear() }} <a href="http://alois-seckar.cz/">Alois Sečkár</a>
     </div>
   </div>
 </template>
