70837f371e4a2e22050e8307679e2acf9f566a4d
Alois Sečkár <ellrohir@seznam.cz>
2024-01-05 18:49:37 +0100

    feat: JavaNews and NuxtNews digests

---
+++ components/NewsJava.vue
@@ -0,0 +1,29 @@
+<template>
+  <h2>Co nového v Java světě?</h2>
+  <ul class="list-disc articleList">
+    <li v-for="news in javaNews" :key="news.link">
+      <span class="mr-2 text-coda-purple">{{ news.date }}</span>
+      <NuxtLink :to="news.link" :external="true" noprefetch>
+        <strong>{{ news.title }}</strong>
+      </NuxtLink>
+      - {{ news.dscr }}
+    </li>
+  </ul>
+  <div>
+    <NuxtLink to="https://github.com/AloisSeckar/demos-java/blob/master/JavaNews.md" noprefetch>
+      Zobrazit vše
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup>
+const javaNews = []
+const { data } = await useAsyncData('javaNews', () => $fetch('https://alois-seckar.cz/java-news'))
+if (data.value) {
+  javaNews.push(data.value.item1)
+  javaNews.push(data.value.item2)
+  javaNews.push(data.value.item3)
+  javaNews.push(data.value.item4)
+  javaNews.push(data.value.item5)
+}
+</script>
---
+++ components/NewsNuxt.vue
@@ -0,0 +1,29 @@
+<template>
+  <h2>Co nového v Nuxt/Vue světě?</h2>
+  <ul class="list-disc articleList">
+    <li v-for="news in nuxtNews" :key="news.link">
+      <span class="mr-2 text-coda-purple">{{ news.date }}</span>
+      <NuxtLink :to="news.link" :external="true" noprefetch>
+        <strong>{{ news.title }}</strong>
+      </NuxtLink>
+      - {{ news.dscr }}
+    </li>
+  </ul>
+  <div>
+    <NuxtLink to="https://github.com/AloisSeckar/demos-nuxt/blob/main/NuxtNews.md" noprefetch>
+      Zobrazit vše
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup>
+const nuxtNews = []
+const { data } = await useAsyncData('nuxtNews', () => $fetch('https://alois-seckar.cz/nuxt-news'))
+if (data.value) {
+  nuxtNews.push(data.value.item1)
+  nuxtNews.push(data.value.item2)
+  nuxtNews.push(data.value.item3)
+  nuxtNews.push(data.value.item4)
+  nuxtNews.push(data.value.item5)
+}
+</script>
