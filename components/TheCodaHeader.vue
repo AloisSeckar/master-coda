6b1c6c313aaa158c0808d16c5e5523488a69896e
Alois Sečkár <ellrohir@seznam.cz>
2024-03-09 18:02:59 +0100

    feat: hamburger menu for narrow screens

--- components/TheCodaHeader.vue
+++ components/TheCodaHeader.vue
@@ -64,6 +64,54 @@
         <div :class="headerH2">
           Nepravidelný blog o&nbsp;programování
         </div>
+        <div class="absolute top-4 right-4 md:right-[10%] mr-1">
+          <button id="menuBtn" class="block hamburger focus:outline-none" @click="menu = !menu">
+            <div class="space-y-2">
+              <span class="block w-8 h-0.5 bg-coda-yellow" />
+              <span class="block w-8 h-0.5 bg-coda-yellow" />
+              <span class="block w-8 h-0.5 bg-coda-yellow" />
+            </div>
+          </button>
+        </div>
+        <div v-if="menu" class="absolute top-12 right-4 md:right-[10%] mr-1 border-2 border-coda-green bg-slate-400">
+          <ul>
+            <li :class="menuButtonFloat">
+              <NuxtLink :to="{ path: '/category/java' }">
+                <div :class="menuFont">
+                  JAVA
+                </div>
+              </NuxtLink>
+            </li>
+            <li :class="menuButtonFloat">
+              <NuxtLink :to="{ path: '/category/web' }">
+                <div :class="menuFont">
+                  WEB
+                </div>
+              </NuxtLink>
+            </li>
+            <li :class="menuButtonFloat">
+              <NuxtLink :to="{ path: '/category/debugging' }">
+                <div :class="menuFont">
+                  DEBUGGING
+                </div>
+              </NuxtLink>
+            </li>
+            <li :class="menuButtonFloat">
+              <NuxtLink :to="{ path: '/category/misc' }">
+                <div :class="menuFont">
+                  OSTATNÍ
+                </div>
+              </NuxtLink>
+            </li>
+            <li :class="menuButtonFloat">
+              <NuxtLink :to="{ path: '/fun' }">
+                <div :class="menuFont">
+                  HUMOR
+                </div>
+              </NuxtLink>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -74,5 +122,8 @@ const header = 'ml-32 h-32 pt-2 border-b-2 border-coda-green'
 const headerH1 = 'ml-1 font-header text-coda-yellow text-2xl'
 const headerH2 = 'ml-1 font-header text-coda-yellow text-xl'
 const menuButton = 'actionButton p-1 w-1/5'
+const menuButtonFloat = 'actionButton p-1 w-36 text-center'
 const menuFont = 'actionButtonText font-bold'
+
+const menu = ref(false)
 </script>
